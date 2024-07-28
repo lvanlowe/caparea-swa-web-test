ed898a126ff651118e533d30adce65d8dfd4d7b7
lvanlowe <lvanlowe@inrvisions.com>
2024-07-27 20:39:32 -0400

    fix: correct name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('basic-sample app is running!');
+    expect(page.getTitleText()).toEqual('caparea-swa-web-test app is running!');
   });
 
   afterEach(async () => {
